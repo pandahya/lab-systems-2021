a929e159968904877e3806252d33d78cb23ce02d
hyacinth <hyacinthweng@gmail.com>
2021-12-07 14:42:59 -0500

    added cart add

--- mikea/js/main.js
+++ mikea/js/main.js
@@ -1,13 +1,27 @@
-const frontDoor = document.getElementById("frontdoor");
-const backDoor = document.getElementById("backdoor");
-const welcome = document.getElementById("welcome");
+const addPlant = document.getElementById("add_plant");
+const addToCart = document.getElementsByClassName("add_to_cart");
+const cartNum = document.getElementById("cart_num");
 
-//https://codepen.io/am_eu/pen/EgZdaQ
-frontDoor.addEventListener("mouseenter", function(){
-    welcome.style.opacity = 1;
-    backDoor.classList.add("doorOpen");
-})
-frontDoor.addEventListener("mouseleave", function(){
-    welcome.style.opacity = 0;
-    backDoor.classList.remove("doorOpen");
-})
+let cartItems = [];
+let cartPrices = [];
+
+//get number of plants in storage at start
+let plantNum = sessionStorage.length - 1;
+
+cartNum.innerHTML = `cart (${sessionStorage.length-1})`;
+
+//for loop to add that number of plants into the array
+for(let i=0; i<plantNum; i++){
+    cartItems.push("plant");
+    cartPrices.push("200");
+}
+
+addPlant.onclick = function(){
+    cartItems.push("plant");
+    cartPrices.push("200");
+    sessionStorage.setItem("plant"+cartItems.length, "200");
+    // sessionStorage.clear();
+
+    cartNum.innerHTML = `cart (${sessionStorage.length-1})`;
+
+}

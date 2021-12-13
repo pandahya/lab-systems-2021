87a61f5230b3c50fe474cdb20f528b7109785dfb
hyacinth <hyacinthweng@gmail.com>
2021-12-12 23:29:57 -0500

    added acct/login

--- mikea/js/cart.js
+++ mikea/js/cart.js
@@ -1,3 +1,12 @@
 const cartNum = document.getElementById("cart_num");
 let plantNum = sessionStorage.length;
 cartNum.innerHTML = `cart (${sessionStorage.length})`;
+
+const menuLogin = document.getElementById("menuLogin");
+let loginState = localStorage.getItem("login");
+
+if(loginState === "true"){
+    menuLogin.innerHTML = "Hi, Tanvi";
+}else if(loginState === "false"){
+    menuLogin.innerHTML = "login";
+}
